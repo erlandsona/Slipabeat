96ab9b521ca629c3da11cfe2d7d3d6449ff8fe93
Austin Erlandson <austin@erlandson.com>
2015-05-02 04:49:35 -0500

    Added usb call to get connected devices:)

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   var h1 = document.getElementsByTagName('h1');
   if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + '   It\'s Working! It\'s Working!';
+    h1[0].innerText = h1[0].innerText + '   It\'s Working! It\'s Working! Please check the Console to make sure a device is connected:)';
   }
 }, false);
+
+
+function onDeviceFound (devices) {
+  console.log(devices[0]);
+}
+
+chrome.usb.getDevices({}, onDeviceFound);
