031911d880efbd6a8baa84900d84a090b5fdb29a
Austin Erlandson <austin@erlandson.com>
2015-05-03 16:38:27 -0500

    JSON Object Parsed and console.logging!!!

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,58 @@
 'use strict';
 
-var leftToeSound = document.querySelector('#left-toe-sound');
-var rightToeSound = document.querySelector('#right-toe-sound');
-var leftHeelSound = document.querySelector('#left-heel-sound');
-var rightHeelSound = document.querySelector('#right-heel-sound');
+var leftToeSound    = document.querySelector('#left-toe-sound'),
+    rightToeSound   = document.querySelector('#right-toe-sound'),
+    leftHeelSound   = document.querySelector('#left-heel-sound'),
+    rightHeelSound  = document.querySelector('#right-heel-sound'),
+    leftToeSelect   = document.querySelector('#left-toe'),
+    rightToeSelect  = document.querySelector('#right-toe'),
+    leftHeelSelect  = document.querySelector('#left-heel'),
+    rightHeelSelect = document.querySelector('#right-heel'),
+    SERIAL          = chrome.serial,
+    serialObj       = {},
+    connectionInfo,
+    connectionId;
 
-var leftToeSelect = document.querySelector('#left-toe');
-var rightToeSelect = document.querySelector('#right-toe');
-var leftHeelSelect = document.querySelector('#left-heel');
-var rightHeelSelect = document.querySelector('#right-heel');
+
+
+function onGetDevices (ports) {
+  chrome.serial.connect(ports[5].path, function onConnect(info) {
+    connectionId = info.connectionId;
+    connectionInfo = info;
+  });
+}
+
+var str = '';
+
+function onReceiveCallback (arrayBuffer) {
+  if (!!str.match(/'/g) && str.match(/'/g).length === 2) {
+    console.log(str);
+    serialObj = JSON.parse(str.replace(/'/g, ''));
+    str = '';
+  }
+  var u8view = new Uint8Array(arrayBuffer.data);
+  for (var i = 0, len = u8view.length; i < len; i++) {
+    str += String.fromCharCode(u8view[i]);
+  }
+}
+
+function onReceiveErrorCallback (info) {
+  console.log(info);
+}
+
+
+SERIAL.getDevices(onGetDevices);
+SERIAL.onReceive.addListener(onReceiveCallback);
+SERIAL.onReceiveError.addListener(onReceiveErrorCallback);
+
+
+
+
+
+
+
+
+
 
 leftToeSelect.onchange = function() {
   leftToeSound.src = 'assets/TR808WAV/' + leftToeSelect.value;
@@ -40,19 +84,5 @@ document.querySelector('#play-right-heel').onclick = function() {
   rightHeelSound.play();
 };
 
-//onLeftToeTap = function() {
-  //leftToeSound.load();
-  //leftToeSound.play();
-//};
-//onRightToeTap = function() {
-  //rightToeSound.load();
-  //rightToeSound.play();
-//};
-//onLeftHeelTap = function() {
-  //leftHeelSound.load();
-  //leftHeelSound.play();
-//};
-//onRightHeelTap = function() {
-  //rightHeelSound.load();
-  //rightHeelSound.play();
-//};
+
+
