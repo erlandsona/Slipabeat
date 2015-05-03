34d0200157b53ef4aa05a900c7ff8677e9ba4ed9
Austin Erlandson <austin@erlandson.com>
2015-05-03 01:12:35 -0500

    Styling up the page:)

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,51 +1,37 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + '   It\'s Working! It\'s Working! Please check the Console to make sure a device is connected:)';
-  }
-}, false);
 
+var leftToeSound = document.querySelector('#left-toe-sound');
+var rightToeSound = document.querySelector('#right-toe-sound');
+var leftHeelSound = document.querySelector('#left-heel-sound');
+var rightHeelSound = document.querySelector('#right-heel-sound');
 
-function onDeviceFound (devices) {
-  console.log(devices[0]);
-}
-
-//chrome.usb.getDevices({}, onDeviceFound);
-
-
-var leftToeSound = document.querySelector("#left-toe-sound");
-var rightToeSound = document.querySelector("#right-toe-sound");
-var leftHeelSound = document.querySelector("#left-heel-sound");
-var rightHeelSound = document.querySelector("#right-heel-sound");
-
-var leftToeSelect = document.querySelector("#left-toe");
-var rightToeSelect = document.querySelector("#right-toe");
-var leftHeelSelect = document.querySelector("#left-heel");
-var rightHeelSelect = document.querySelector("#right-heel");
+var leftToeSelect = document.querySelector('#left-toe');
+var rightToeSelect = document.querySelector('#right-toe');
+var leftHeelSelect = document.querySelector('#left-heel');
+var rightHeelSelect = document.querySelector('#right-heel');
 
 leftToeSelect.onchange = function() {
-  leftToeSound.src = "assets/TR808WAV/" + leftToeSelect.value;
-}
+  leftToeSound.src = 'assets/TR808WAV/' + leftToeSelect.value;
+};
 rightToeSelect.onchange = function() {
-  rightToeSound.src = "assets/TR808WAV/" + rightToeSelect.value;
-}
+  rightToeSound.src = 'assets/TR808WAV/' + rightToeSelect.value;
+};
 leftHeelSelect.onchange = function() {
-  leftHeelSound.src = "assets/TR808WAV/" + leftHeelSelect.value;
-}
+  leftHeelSound.src = 'assets/TR808WAV/' + leftHeelSelect.value;
+};
 rightHeelSelect.onchange = function() {
-  rightHeelSound.src = "assets/TR808WAV/" + rightHeelSelect.value;
-}
+  rightHeelSound.src = 'assets/TR808WAV/' + rightHeelSelect.value;
+};
 
-document.querySelector("#play-left-toe").onclick = function() {
+document.querySelector('#play-left-toe').onclick = function() {
   leftToeSound.play();
-}
-document.querySelector("#play-right-toe").onclick = function() {
+};
+document.querySelector('#play-right-toe').onclick = function() {
   rightToeSound.play();
-}
-document.querySelector("#play-left-heel").onclick = function() {
+};
+document.querySelector('#play-left-heel').onclick = function() {
   leftHeelSound.play();
-}
-document.querySelector("#play-right-heel").onclick = function() {
+};
+document.querySelector('#play-right-heel').onclick = function() {
   rightHeelSound.play();
-}
+};
