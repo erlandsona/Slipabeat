a31e9dd504bb57e2f3f7ca1a0d16db658785d664
Noah Yarian <noah.yarian@gmail.com>
2015-05-03 09:56:32 -0500

    added commented-out pseudocode event listener

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -24,14 +24,35 @@ rightHeelSelect.onchange = function() {
 };
 
 document.querySelector('#play-left-toe').onclick = function() {
+  leftToeSound.load();
   leftToeSound.play();
 };
 document.querySelector('#play-right-toe').onclick = function() {
+  rightToeSound.load();
   rightToeSound.play();
 };
 document.querySelector('#play-left-heel').onclick = function() {
+  leftHeelSound.load();
   leftHeelSound.play();
 };
 document.querySelector('#play-right-heel').onclick = function() {
+  rightHeelSound.load();
   rightHeelSound.play();
 };
+
+//onLeftToeTap = function() {
+  //leftToeSound.load();
+  //leftToeSound.play();
+//};
+//onRightToeTap = function() {
+  //rightToeSound.load();
+  //rightToeSound.play();
+//};
+//onLeftHeelTap = function() {
+  //leftHeelSound.load();
+  //leftHeelSound.play();
+//};
+//onRightHeelTap = function() {
+  //rightHeelSound.load();
+  //rightHeelSound.play();
+//};
