a5329638c1687ef269c0e5868e72140ac667144f
Stephen Kinser <sdkinser456@gmail.com>
2015-05-02 22:43:46 -0500

    debounce function merge

--- serialWrite/serialWrite.ino
+++ serialWrite/serialWrite.ino
@@ -1,14 +1,14 @@
-
-int treble= 0;
-int bass=1;
-int trebleo=2;
-int basso=3;
 int inputs=4;
 
-byte val[4] = {0,0,0,0};
+int val[4] = {0,0,0,0};
 int statePin = LOW;
-int THRESHOLD = 100;//IMPORTANT FOR SENSITIVITY
-
+int THRESHOLD = 255;//IMPORTANT FOR SENSITIVITY
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 100;    // the debounce time; increase if the output flickers
+//int outState[4]= {HIGH,HIGH,HIGH,HIGH};
+bool hit;
+int currentState[4];
+int lastState[4]= {LOW,LOW,LOW,LOW};
 void setup() {
   pinMode(0, INPUT);//leftTre
   pinMode(1, INPUT);//leftBass
@@ -18,10 +18,18 @@ void setup() {
   
 }
 void loop() {
-bool hit= false;
-String jsonString="\"{";
+  hit= false;
+  int currReading[4]= {0,0,0,0};
+String jsonString="\"{" ;
 for (int i=0;i<inputs;i++){
- val[i]= analogRead(i); 
+/* if((byte)analogRead(i) >= THRESHOLD){
+  Serial.println((byte)analogRead(i));
+   currReading[i]= HIGH;
+}
+   else{
+    currReading[i]= LOW; 
+   }
+  */ 
   String key;
    switch(i){
      case 0:
@@ -40,29 +48,39 @@ for (int i=0;i<inputs;i++){
      key="Bad";
      break;
      }//end switch
-
- if(val[i] >= THRESHOLD && val[i] != 255){ //value read
-     hit = true;
+   if ((millis() - lastDebounceTime) > debounceDelay) {
+    if((byte)analogRead(i)>=THRESHOLD&& lastState[i]== LOW){
+      hit= true;
      jsonString= jsonString + "\'" + key + "\': 1" ;
- }//endif
- else{ // value not read
+     lastState[i]=HIGH; 
+    }
+   else if((byte)analogRead(i)< 50){ // value not read
    jsonString= jsonString +  "\'" + key + "\': 0" ;
-     
- }//end else
- if(i == inputs-1)
-   jsonString= jsonString + "}\" \n";
+       lastState[i]=LOW;      
+   }//end else
+   else{
+     jsonString= jsonString +  "\'" + key + "\': 0" ;
+   }
+  }
  
- else
+ 
+ if(i == inputs-1){
+   jsonString= jsonString + "}\" \n";}
+ else{
   jsonString = jsonString +", "; 
- 
+}
+
 }//endfor
+//delay(100);
   char jsonOut[100]="";
   if(hit==true){
     jsonString.toCharArray(jsonOut, jsonString.length());
-    Serial.write(jsonOut);
-  //Serial.println(jsonString);
-  
+   //Serial.write(jsonOut);
+  /*for (int i=0;i<inputs;i++){
+  Serial.println(currReading[i]);
+  }*/
+  Serial.println(jsonString);
 }
-delay(100);
+delay(100);  
   
 }
